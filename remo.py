7349799c34257a26839f9c08d951fc763d454115
Synth <adlicabulo@gmail.com>
2023-04-20 22:17:41 +0700

    Update remo.py
    1. Imports only the required functions from the utils module, making the code cleaner and more explicit.
    2. Adds type hints to the return types of the endpoints for better clarity and type safety.
    3. Renames the endpoints rebuild_tree and maintain_tree to avoid naming conflicts with the imported functions from utils.

--- remo.py
+++ remo.py
@@ -1,42 +1,42 @@
-from fastapi import FastAPI
-import utils
 import os
+from fastapi import FastAPI
+from utils import create_message, save_message, search_tree, rebuild_tree, maintain_tree
 
 app = FastAPI()
 root_folder = os.getcwd()
-#root_folder = 'C:/raven_private/REMO/'
 max_cluster_size = 5
+#root_folder = 'C:/raven_private/REMO/'
 # REMO = Rolling Episodic Memory Organizer
 
 @app.post("/add_message")
-async def add_message(message: str, speaker: str, timestamp: float):
+async def add_message(message: str, speaker: str, timestamp: float) -> dict:
     # Add message to REMO
-    new_message = utils.create_message(message, speaker, timestamp)
-    print('\n\nADD MESSAGE -', new_message)
-    utils.save_message(root_folder, new_message)
+    new_message = create_message(message, speaker, timestamp)
+    print(f"\n\nADD MESSAGE - {new_message}")
+    save_message(root_folder, new_message)
 
     return {"detail": "Message added"}
 
 @app.get("/search")
-async def search(query: str):
+async def search(query: str) -> dict:
     # Search the tree for relevant nodes
-    print('\n\nSEARCH -', query)
-    taxonomy = utils.search_tree(root_folder, query)
+    print(f"\n\nSEARCH - {query}")
+    taxonomy = search_tree(root_folder, query)
 
     return {"results": taxonomy}
 
 @app.post("/rebuild_tree")
-async def rebuild_tree():
-    # Trigger full tree rebuilding event
-    print('\n\nREBUILD TREE')
-    utils.rebuild_tree(root_folder, max_cluster_size)
+async def rebuild_tree_endpoint() -> dict:
+     # Trigger full tree rebuilding event
+    print("\n\nREBUILD TREE")
+    rebuild_tree(root_folder, max_cluster_size)
 
     return {"detail": "Tree rebuilding completed"}
 
 @app.post("/maintain_tree")
-async def maintain_tree():
-    # Trigger tree maintenance event
-    print('\n\nMAINTAIN TREE')
-    utils.maintain_tree(root_folder)
+async def maintain_tree_endpoint() -> dict:
+     # Trigger tree maintenance event
+    print("\n\nMAINTAIN TREE")
+    maintain_tree(root_folder)
 
     return {"detail": "Tree maintenance completed"}
